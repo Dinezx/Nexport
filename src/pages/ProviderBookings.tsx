9011c1612adf87d1526ffa0e52303846b41b61ec
Dinezx <dineshkumarsaj@gmail.com>
2026-02-25 21:44:49 +0530

    Refactor UI section comment formatting in ProviderBookings component

--- src/pages/ProviderBookings.tsx
+++ src/pages/ProviderBookings.tsx
@@ -86,7 +86,7 @@ export default function ProviderBookings() {
     fetchBookings();
   }, []);
 
-  /* ---------------- UI ---------------- */
+/* ---------------- UI ---------------- */
 
   const statusColor = (status: string) => {
     switch (status) {
