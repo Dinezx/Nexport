36b72360df6b3bd36633671be532effa5077713c
Dinezx <dineshkumarsaj@gmail.com>
2026-03-04 10:25:15 +0530

    feat: update package.json and vite.config.ts for deployment and routing improvements

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 import { AuthProvider } from "./context/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Nexport">
   <AuthProvider>
     <App />
   </AuthProvider>
